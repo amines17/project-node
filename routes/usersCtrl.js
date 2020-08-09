8bee712e1eb41ce03c4e2d5c2c7f5c07074a5727
Amine <amine.soufi93@gmail.com>
2020-08-09 14:56:27 +0200

    login/register + ctrl + JWT

--- routes/usersCtrl.js
+++ routes/usersCtrl.js
@@ -16,12 +16,12 @@ module.exports = {
             return res.status(400).json({ 'ereur': 'paramètres manquant'});
         }
 
-        models.User.findOne({
-            attributes: ['email'],
-            where: { email: email }
+        models.User.findOne({  // On verifie si l'utilisateur existe
+            attributes: ['email'], // Conditon
+            where: { email: email } // Conditon
         })
         .then(function(userFound){
-            if(!userFound){
+            if(!userFound){ // Si l'on ne trouve pas d'utilisateur avec cette email alors on le crée
                 bcrypt.hash(password, 5, function( err, bcryptedPassword){
                     var newUser = models.User.create({
                         email: email,
@@ -30,7 +30,7 @@ module.exports = {
                         bio: bio,
                         isAdmin: 0
                     })
-                    .then(function(newUser){
+                    .then(function(newUser){ // Création de l'utilisateur 
                         return res.status(201).json({
                             'userId': newUser.id
                         })
@@ -57,19 +57,19 @@ module.exports = {
             return res.status(400).json({ 'ereur': 'paramètres manquant'});
         }
 
-        models.User.findOne({
-            where: { email : email }
+        models.User.findOne({ // On verifie si l'utilisateur existe
+            where: { email : email } // Condition
         })
-        .then(function(userFound){
+        .then(function(userFound){ // Si l'on trouve un utilisateur alors :
             if(userFound) {
-                bcrypt.compare(password, userFound.password, function(errBycrypt, resBycrypt){
+                bcrypt.compare(password, userFound.password, function(errBycrypt, resBycrypt){ // comparaison des mdp crypté
                     if(resBycrypt){
                         return res.status(200).json({ 
                             'userId': userFound.id, 
                             'token': jwtUtils.generateTokenForUser(userFound)
                         });
                     } else {
-                        return res.status(403).json({ 'erreur': 'mot de passe incorrect ! ' });
+                        return res.status(403).json({ 'erreur': 'mot de passe incorrect ! ' }); 
                     }
                 });      
             } else {
